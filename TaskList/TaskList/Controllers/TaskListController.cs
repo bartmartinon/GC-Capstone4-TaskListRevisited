06fa2e2626d657efb05493676333d8db09950ce8
Bart Martinon <barthelemy.martinon@gmail.com>
2020-11-18 11:25:59 -0500

    Added ability to create new Users, might be running into bug once trying to log in with new user after having made them

--- TaskList/TaskList/Controllers/TaskListController.cs
+++ TaskList/TaskList/Controllers/TaskListController.cs
@@ -25,13 +25,11 @@ namespace TaskList.Controllers
         [HttpPost]
         public IActionResult LogIn(string Email, string Password)
         {
-            bool UserAuth = false;
             List<User> Users = _db.Users.ToList();
             foreach (User u in Users)
             {
                 if (Email.Equals(u.Email) && Password.Equals(u.Password))
                 {
-                    UserAuth = true;
                     ViewBag.UserId = u.Id;
                     ViewBag.logMsg = $"User ID{u.Id} found!";
                     return View();
@@ -40,6 +38,38 @@ namespace TaskList.Controllers
             }
             ViewBag.logMsg = "Incorrect Email/Password";
             return View(Users);
+        }
+
+        public IActionResult NewUser()
+        {
+            ViewBag.logMsg = "Ready";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NewUser(User U)
+        {
+            if (ModelState.IsValid)
+            {
+                List<User> Users = _db.Users.ToList();
+                foreach (User existingUser in Users)
+                {
+                    if (existingUser.Email == U.Email)
+                    {
+                        ViewBag.logMsg = $"User {U.Email} already in system. Please log in instead.";
+                        return View("NewUser");
+                    }
+                }
+                _db.Users.Add(U);
+                _db.SaveChanges();
+                ViewBag.logMsg = $"New User {U.Email} added to system! You can now log in!";
+                return View("LogIn");
+            }
+            else
+            {
+                ViewBag.logMsg = "Invalid format.";
+                return View("NewUser");
+            }
         }
     }
 }
--- TaskList/TaskList/Models/User.cs
+++ TaskList/TaskList/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,7 +14,10 @@ namespace TaskList.Models
         }
 
         public int Id { get; set; }
+        [StringLength(50, ErrorMessage = "Email cannot be more than 50 characters long.")]
         public string Email { get; set; }
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 50 characters long.")]
+        //[RegularExpression(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!#$%^&-+=()])(?=\\S+$).{8, 50}$", ErrorMessage = "Password does not meet requirements, please review and try again.")]
         public string Password { get; set; }
 
         public virtual ICollection<ToDoItem> ToDoItems { get; set; }
